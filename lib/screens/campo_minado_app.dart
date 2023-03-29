bf27734cab3fc96b809bb184ee6a25eefd4b2b26
faamaral <fabianoamaral445@gmail.com>
2023-03-29 11:26:08 -0300

    criado tela principal da aplicação

---
+++ lib/screens/campo_minado_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CampoMinadoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Campo Minado')),
+        body: Container(child: Text('Tabuleiro'),),
+      ),
+    );
+  }
+}
